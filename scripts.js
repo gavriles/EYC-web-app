5eddc7b6aaa92db54be39f96b9d3e322bf7d81c5
gavriles <ahsaira@tutamail.com>
2024-06-06 21:44:36 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-// Плавная прокрутка к разделу "Приключения"
-function scrollToAdventures() {
-    document.getElementById('adventures').scrollIntoView({ behavior: 'smooth' });
-}
+// scripts.js
+
+document.querySelector('button').addEventListener('click', function() {
+    // Code to handle contact form or booking process goes here
+    // This is just a placeholder function
+    alert('Contact form or booking process will be handled here!');
+});
